a69f4d0e2fa385d83cda1d911fdeacd023bb28a1
Rozana-IM <rozanaim09@gmail.com>
2026-02-06 21:19:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/users", (req, res) => {
+  res.json([
+    { id: 1, name: "Rozana" },
+    { id: 2, name: "DevOps User" }
+  ]);
+});
+
+app.get("/health", (req, res) => {
+  res.send("User Service is healthy");
+});
+
+app.listen(PORT, () =>
+  console.log(`User Service running on port ${PORT}`)
+);
+
+db.js:
+-------
+module.exports = {
+  connect: () => {
+    console.log("User Service DB connected");
+  }
+};
 
