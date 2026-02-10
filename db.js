28fb17986b75cf04383bf35bda445a53d3223ffa
Rozana-IM <rozanaim09@gmail.com>
2026-02-10 19:32:22 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
-module.exports = {
-  connect: () => {
-    console.log("User Service DB connected");
-  }
-};
+DB_HOST=database-2.cq5o8mwo25nj.us-east-1.rds.amazonaws.com
+DB_USER=admin
+DB_PASSWORD=e6Rufsl3lKw3WNXjh2yK
+DB_NAME=database-2
+DB_POST=3306
