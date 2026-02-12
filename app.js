edca6f3d09c30be384d2b4ae997d29f7591c960c
Rozana-IM <rozanaim09@gmail.com>
2026-02-12 17:38:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,19 @@
 const express = require("express");
-
 const cors = require("cors");
- 
 const app = express();
-
 const PORT = process.env.PORT || 4000;
- 
 app.use(cors({
-
   origin: "https://rozana-projects.online", // replace with your real domain
-
 }));
- 
 app.get("/users", (req, res) => {
-
   res.json([
-
     { id: 1, name: "Siva" },
-
     { id: 2, name: "DevOps User" }
-
   ]);
-
 });
- 
 app.get("/health", (req, res) => {
-
   res.send("User Service is healthy");
-
 });
- 
 app.listen(PORT, "0.0.0.0", () => {
-
   console.log(`User Service running on port ${PORT}`);
-
 });
