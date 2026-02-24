ca62cd789479a9e20b981128260012b7ee1dc286
Rozana-IM <rozanaim09@gmail.com>
2026-02-24 23:06:04 +0530

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,20 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {
-  registerUser,
-  loginUser,
-  getAllUsers,
-} = require("../controllers/auth.controller");
+const ctrl = require("../controllers/auth.controller");
 
-const {
-  verifyToken,
-  isAdmin,
-} = require("../middleware/auth.middleware");
-
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-
-// 🔐 ADMIN PROTECTED
-router.get("/admin/users", verifyToken, isAdmin, getAllUsers);
+router.post("/register", ctrl.registerUser);
+router.post("/login", ctrl.loginUser);
+router.post("/auth/refresh", ctrl.refreshToken);
 
 module.exports = router;
