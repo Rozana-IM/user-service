86bd375a110272100ab1584a32d8d7b53273573d
Rozana-IM <rozanaim09@gmail.com>
2026-02-24 15:27:04 +0530

    Add authentication routes for user registration and login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const router = express.Router();
 
+const {
+  registerUser,
+  loginUser,
+  getAllUsers,
+} = require("../controllers/auth.controller");
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/admin/users", getAllUsers);
+
+module.exports = router;
