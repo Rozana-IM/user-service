81c592a2bcbf83e84913a1fd354615bd1986ad8d
Rozana-IM <rozanaim09@gmail.com>
2026-02-25 21:37:46 +0530

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const ctrl = require("../controllers/auth.controller");
 
-router.post("/register", ctrl.registerUser);
-router.post("/login", ctrl.loginUser);
-router.post("/auth/refresh", ctrl.refreshToken);
+const authController = require("../controllers/auth.controller");
+
+// ================= AUTH =================
+router.post("/register", authController.registerUser);
+router.post("/login", authController.loginUser);
+router.post("/auth/refresh", authController.refreshToken);
 
 module.exports = router;
