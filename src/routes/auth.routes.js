5d6a252a6651cf8702a3034ce066475fa692dcd5
Rozana-IM <rozanaim09@gmail.com>
2026-03-03 21:33:37 +0530

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const authController = require("../controllers/auth.controller");
 const { verifyToken } = require("../middleware/auth.middleware");
 
-// ================= AUTH =================
+// AUTH
 router.post("/register", authController.registerUser);
 router.post("/login", authController.loginUser);
 router.post("/auth/refresh", authController.refreshToken);
 
-// ================= ADMIN =================
+// ADMIN USERS
 router.get("/users", verifyToken, authController.getAllUsers);
 
 module.exports = router;
