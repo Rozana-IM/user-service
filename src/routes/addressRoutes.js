e4eb1f2ff4b26c3ba94cedcd8ddf753abe787f2c
Rozana-IM <rozanaim09@gmail.com>
2026-04-03 15:33:33 +0530

    Update addressRoutes.js

--- src/routes/addressRoutes.js
+++ src/routes/addressRoutes.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const addressController = require("../controllers/addressController");
+const { refreshToken } = require("../controllers/auth.controller"); // ✅ FIX
 
 router.post("/users/:id/address", addressController.addAddress);
 
 router.get("/users/:id/addresses", addressController.getAddresses);
+
+// ✅ REFRESH TOKEN ROUTE
 router.post("/refresh-token", refreshToken);
 
 module.exports = router;
