2f088d3b1b6d41fdd57f46b2e189b37d9326918f
Rozana-IM <rozanaim09@gmail.com>
2026-04-04 18:45:57 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,8 @@ app.use(express.json());
 
 app.use(cors({
   origin: process.env.FRONTEND_URL || "https://rozana-projects.online",
-  methods: ["GET", "POST"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 }));
 
